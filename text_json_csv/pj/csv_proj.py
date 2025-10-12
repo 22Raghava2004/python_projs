d08bc95682ba70703b03d83933cb19a76b902cf7
22Raghava2004 <raghavangowda2004@gmail.com>
2025-10-12 22:33:55 +0530

    kurui

--- text_json_csv/pj/csv_proj.py
+++ text_json_csv/pj/csv_proj.py
@@ -1,18 +1,29 @@
 import json
 import csv
-employee=[["name","age","job"],
-          ["spongebob",20,"fry cook"],
-            ["sandy",30,"scientist"],
-            ["patrick",19,"unemploye "]
+company={}
+
+employees=input("how many employees do you want to add")
+key=0
+
+for i in range(int(employees)):
+    employee_id=input("enter the employee id")
+    employee_name=input("enter the employee name")
+    employee_salary=input("enter the employee salary")
+    company[employee_id]={key:employee_id,"name":employee_name,"salary":employee_salary}
+    key+=1
         
-          ]
+
+
+        
+
 
 text_file="C:\\Users\\ragha\\OneDrive\\Desktop\\output.csv"
 try:
     with open(text_file,"w", newline="") as file:
         writer = csv.writer(file)#writerow is used to write the row in csv files
-        for row in employee:
-            writer.writerow(row)
+        for key,value in company.items():
+            writer.writerow([key, value["name"], value["salary"]])
+
         print(f"the file name {text_file} exist")
     with open(text_file,"r",newline="") as file:
         reader=csv.reader(file)
